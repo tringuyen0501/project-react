a05c636fd826956d9e850f9832486cc37ff4c1f8
tringuyen0501 <tringuyen011995@gmail.com>
2022-11-13 11:22:41 +0700

    update template login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    // Simple POST request with a JSON body using fetch
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: "React POST Request Example" }),
+    };
+    // fetch("https://jsonplaceholder.typicode.com/users", requestOptions)
+    // fetch("https://jsonplaceholder.typicode.com/users")
+      fetch("http://localhost:3000/users")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        });
+      });
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading ... </div>;
+    } else {
+      return (
+        <div className="App">
+          <ul>
+            {items.map((item) => (
+              <li key={item.id}>
+                Name: {item.name} | Email: {item.userId}
+              </li>
+            ))}
+            {/* {items.map((item) => (
+              <li key={item.users}>
+                Name: {item.name} | User Id: {item.userId}
+              </li>
+            ))} */}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
